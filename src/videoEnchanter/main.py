030cda3bb06e58e56a20224b1fe1fcf0e9a02f83
TomDenem1010 <43414435+TomDenem1010@users.noreply.github.com>
2026-05-26 17:04:05 +0200

    NEW: init

---
+++ src/videoEnchanter/main.py
@@ -0,0 +1,16 @@
+import logging;
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    logger.info("Hello World")
+
+
+if __name__ == "__main__":
+    main()
